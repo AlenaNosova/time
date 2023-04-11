074525cd7eb83a2e1ab4349258d58e689442ce84
AlenaNosova <Teplykh91@gmail.com>
2023-04-11 15:07:28 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './App.css'
-import withDateTimePretty from './components/DateTimePretty'
-
-function DateTime(props) {
-    return (
-        <p className="date">{props.date}</p>
-    )
-}
-
-const DateTimePretty = withDateTimePretty(DateTime);
-
-function Video(props) {
-    return (
-        <div className="video">
-            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <DateTimePretty date={props.date} />
-        </div>
-    )
-}
-
-function VideoList(props) {
-    return props.list.map(item => <Video url={item.url} date={item.date} />);
-}
+import VideoList from './components/VideoList'
 
 export default function App() {
-    const [list, setList] = useState([
+    const list = [
         {
             url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
             date: '2017-07-31 13:24:00'
@@ -49,7 +28,7 @@ export default function App() {
             url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
             date: '2017-12-02 05:24:00'
         },
-    ]);
+    ];
 
     return (
         <VideoList list={list} />
